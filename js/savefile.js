0d00c080f9663cb09377ec2194306b88725c38b3
Chris Connett <chris@connett.net>
2016-03-24 02:27:40 -0400

    Reading and parsing the journal looks good.

--- js/savefile.js
+++ js/savefile.js
@@ -1,31 +1,56 @@
-function read() {
-  console.log('start');
+// import jspack from "jspack";
+var jspack = new JSPack();
+
+class Journal {
+  constructor(bytes) {
+    this.caveUnlocked = jspack.Unpack('<I', bytes, 0x218);
+    var [deliveryA, deliveryB] = jspack.Unpack('<II', bytes, 0x21c);
+    this.numShortcutsUnlocked = Math.floor(deliveryA / 2);
+    this.numDeliveries = deliveryA % 2 + deliveryB;
+    this.places = jspack.Unpack('<xxxx' +
+                                    'I'.repeat(10),
+                                bytes, 0x420);
+    this.enemies = jspack.Unpack('<xxxx' +
+                                     'I'.repeat(56),
+                                 bytes, 0x520);
+    this.items = jspack.Unpack('<xxxx' +
+                                   'I'.repeat(34),
+                               bytes, 0x620);
+    this.traps = jspack.Unpack('<xxxx' +
+                                   'I'.repeat(14),
+                               bytes, 0x670);
+  }
+}
+
+function chooseNew() {
   chrome.fileSystem.chooseEntry(
       {
-        'type': 'openDirectory',
+        'type': 'openFile',
         'suggestedName':
-            'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Spelunky\\Data'
+            '/Program Files (x86)/Steam/steamapps/common/Spelunky/Data/spelunky_save.sav'
       },
       function(entry) {
         chrome.storage.local.set(
-            {'spelunkyData': chrome.fileSystem.retainEntry(entry)});
-        console.log(entry);
-        var dirReader = entry.createReader();
-        dirReader.readEntries(function(results) {
-          results.forEach(function(item) {
-            console.log('inspecting ' + item.fullPath);
-            if (item.fullPath.endsWith('\\spelunky_save.sav')) {
-              item.file(function(file) {
-                var reader = new FileReader();
-                reader.onload = function(event) {
-                  console.log(event.target.result);
-                };
-                reader.readAsBinaryString(file);
-              });
-            }
-          });
-        });
+            {'spelunkyJournal': chrome.fileSystem.retainEntry(entry)});
+        openEntryAsJournal(entry);
       });
 }
 
-document.getElementById('findSave').addEventListener('click', read);
+function openEntryKeyAsJournal(key, cc) {
+  chrome.fileSystem.restoreEntry(key, function(entry) {
+    entry.file(function(file) {
+      var reader = new FileReader();
+      reader.onload = function(event) {
+        console.log(event.target.result);
+        cc(new Journal(new Int8Array(event.target.result)));
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  });
+}
+
+function logStoredEntryJournal() {
+  chrome.storage.local.get('spelunkyJournal', function(data) {
+    openEntryKeyAsJournal(data['spelunkyJournal'], a => console.log(a));
+  });
+}
